a85d4ccfdc0e8676c1e3d92ae59e6b1dbb825a05
prat33k-dev <bhujbalpratik97@gmail.com>
2022-05-07 16:43:37 -0400

    added visualization rviz

--- prm_apf_planner/src/utils/rviz_plot.py
+++ prm_apf_planner/src/utils/rviz_plot.py
@@ -2,18 +2,24 @@
 import rospy
 import pandas as pd
 from visualization_msgs.msg import Marker, MarkerArray
-import os
+import rospkg
+from geometry_msgs.msg import Point
 
 
 def main():
-    dir = os.path.dirname(results)
-    filename = os.path.join(dir, '/data.csv')
     rospy.init_node('path')
+    rospack = rospkg.RosPack()
     rospy.loginfo('Publishing Path')
+    fp = rospack.get_path("prm_apf_planner")
     publisher = rospy.Publisher('/paths', MarkerArray, queue_size=1)
-    data = pd.read_csv(filename)
+    data = pd.read_csv(fp + '/results/data.csv')
     Y = data['x'].values
     X = data['y'].values
+    Xn = len(X) + 17 - Y
+    Yn = X
+
+    X = Xn
+    Y = Yn
     markerArray = MarkerArray()
     z = 1.5
     while not rospy.is_shutdown():
@@ -55,7 +61,7 @@ def main():
         marker.scale.y = 0.1
         marker.scale.z = 0.03
         marker.color.a = 1.0
-        marker.color.b = 1.0
+        # marker.color.b = 1.0
         marker.pose.orientation.w = 1.0
         marker.points = []
         for i in range(len(X) - 1):
